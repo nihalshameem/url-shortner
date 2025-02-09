a971d982ef4d3cb6631ffb8eaec087d5e84a10ec
nihalshameem <nihalshameem6@gmail.com>
2025-02-09 22:03:28 +0530

    netlify to main

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
-
-// Your existing Express app code goes here (routes, middleware, etc.)
-app.get('/', (req, res) => {
-  res.send('Hello from your Express app on Netlify!');
-});
+const app = require('../../server'); // Import your Express app from server.js
+// // Your existing Express app code goes here (routes, middleware, etc.)
+// app.get('/', (req, res) => {
+//   res.send('Hello from your Express app on Netlify!');
+// });
 
 // Important: Export the handler function
 exports.handler = serverless(app);
