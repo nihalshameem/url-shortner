d559e17573d54b361fb9029c3095e0710bfaf628
nihalshameem <nihalshameem6@gmail.com>
2025-02-09 22:05:39 +0530

    refactor: import Express app from server.js in Netlify function

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const app = express();
 const app = require('../../server'); // Import your Express app from server.js
 // // Your existing Express app code goes here (routes, middleware, etc.)
 // app.get('/', (req, res) => {
